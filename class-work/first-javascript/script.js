78e23be7bde92491a03216bd2f9da8f51d2f5335
skapur2 <swati.kapur03@gmail.com>
2017-04-02 19:10:10 -0400

    Startup matchmaker hw

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,10 @@
+// document.write(100/10);
+// document.write(50-17);
+// document.write(50+17);
+
+// var first = "Swati"
+// var last = "Kapur"
+
+// document.write(firstlast);
+
+document.querySelector("#title").style.color ="red"
