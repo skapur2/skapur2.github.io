875b53e18ae53e7966a547393de23e9f0040cd18
skapur2 <swati.kapur03@gmail.com>
2017-04-17 18:32:34 -0400

    homework temp calc

--- homework/temp-converter/js/app.js
+++ homework/temp-converter/js/app.js
@@ -1,10 +1,25 @@
 
 
-$('#convertcelsius').click(function(){
+
+$('#farenheit').keydown(function(){
 	var tempFarenheit = $('#farenheit').val();
+	tempFarenheit = parseInt(tempFarenheit)
+	var celsiusSolution = ((tempFarenheit - 32)/ 1.8); 
+	$('#celsius').val(celsiusSolution);
+
+	if (tempFarenheit < 70) {
+		$('body').css('background-color', 'blue');
+	}
+	else  {
+		$('body').css('background-color', 'red');
+	}
+		
+});
+
+$('#celsius').keydown(function(){
 	var tempCelsius = $('#celsius').val();
-	var celsiusSolution = ((parseInt(tempFarenheit)) - 32)/ 1.8; 
-$('#solution').text(celsiusSolution);
-	var farenheitSolution = ((parseInt(tempFarenheit)) - 32)/ 1.8; 
-$('#solution').text(celsiusSolution);
-})
+	var farenheitSolution = ((parseInt(tempCelsius) * 1.8) + 32); 
+$('#farenheit').val(farenheitSolution);
+
+});
+
